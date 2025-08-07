0fb56c6482c9ca355d1a710e11d7fb9c138c3794
Rem Mora <remortalite@gmail.com>
2025-08-08 00:57:26 +0700

    Update Swagger with custom oas specification

--- orders/app.py
+++ orders/app.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+import yaml
 
 from orders.api import api
 
-app = FastAPI()
+app = FastAPI(
+    debug=True, openapi_url='/openapi/orders.json', docs_url='/docs/orders'
+)
+
+oas_doc = yaml.safe_load(
+    (Path(__file__).parent / '../oas.yml').read_text()
+)
+
+app.openapi = lambda: oas_doc
 
 app.include_router(api.app)
 
